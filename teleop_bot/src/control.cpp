ac3b16db22183eefff3fbbe2ddb6791c6533fdb4
Dev <deven.patel2@gmail.com>
2016-10-26 00:10:00 +0530

    add unit step params to launchfile

--- teleop_bot/src/control.cpp
+++ teleop_bot/src/control.cpp
@@ -3,26 +3,30 @@
 #include "geometry_msgs/Twist.h"
 #include <boost/bind.hpp>
 
+struct UnitStep{
+    float linear = 1;
+    float angular = 1;
+};
 
-void keyboardCallback(const std_msgs::Char::ConstPtr& msg, ros::Publisher pub){ 
+void keyboardCallback(const std_msgs::Char::ConstPtr& msg, ros::Publisher pub, UnitStep unit_step){ 
     geometry_msgs::Twist cmd_msg;
 
     switch(msg->data){
         case 'C' : //left arrow
-            ROS_INFO("right arrow");
-            cmd_msg.angular.z = -1; 
+            ROS_DEBUG("right arrow");
+            cmd_msg.angular.z = -1*unit_step.angular; 
             break;
         case 'A' : //up arrow
-            ROS_INFO("up arrow"); 
-            cmd_msg.linear.x = 1; 
+            ROS_DEBUG("up arrow"); 
+            cmd_msg.linear.x = unit_step.linear; 
             break;
         case 'D' ://right arrow
-            ROS_INFO("left arrow"); 
-            cmd_msg.angular.z = 1; 
+            ROS_DEBUG("left arrow"); 
+            cmd_msg.angular.z = unit_step.angular; 
             break;
         case 'B' : //down arrow
-            ROS_INFO("down arrow"); 
-            cmd_msg.linear.x = -1; 
+            ROS_DEBUG("down arrow"); 
+            cmd_msg.linear.x = -1*unit_step.linear; 
             break;
         default :
             break;
@@ -35,7 +39,10 @@ int main(int argc, char **argv){
     ros::init(argc, argv, "control");
     ros::NodeHandle n;
     ros::Publisher command_pub = n.advertise<geometry_msgs::Twist>("cmd_vel", 1);
-    ros::Subscriber sub = n.subscribe<std_msgs::Char>("keyboard", 1, boost::bind(keyboardCallback, _1, command_pub));
+    struct UnitStep unit_step;
+    n.param<float>("unit_linear", unit_step.linear, 1.0);
+    n.param<float>("unit_angular",unit_step.angular, 1.0);
+    ros::Subscriber sub = n.subscribe<std_msgs::Char>("keyboard", 1, boost::bind(keyboardCallback, _1, command_pub, unit_step));
     ros::spin();
     return 0;
 }
